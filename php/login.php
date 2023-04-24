d58b098893a604d9eaba47f873ff621032b01df9
elin57 <112911997+elin57@users.noreply.github.com>
2023-04-24 09:49:59 +0200

    Wrote different messages to send back from the server

--- php/login.php
+++ php/login.php
@@ -3,7 +3,7 @@
 If it's the first time they log in, render mood page, 
 otherwise, render feed page.
 Check key in JSON file? If set to true or false, for example: 
-loggedInBefore: true/false
+firstTime: true/false
 */ 
 require_once "functions.php";
 
@@ -19,14 +19,24 @@ for($i = 0; $i < count($usersArray); $i++) {
     $user = $usersArray[$i];
     $arrayUsername = $usersArray[$i]["username"];
     $arrayPassword = $usersArray[$i]["password"];
-    //$loggedInBefore = $usersArray[$i]["loggedInBefore"];
+    //$firstTime = $usersArray[$i]["firstTime"];
 
     if($loginUsername == $arrayUsername && $loginPassword == $arrayPassword) {
-        if($loggedInBefore == false) {
-            $message = ["username" => $user["username"], "loggedInBefore" => false];
-    
-        }        
+        if($firstTime == true) {
+            $message = ["id" => $user["id"], "username" => $user["username"], "firstTime" => true];
+            
+            $usersArray[$i]["firstTime"] = false;
+            $usersArrayJSON = json_encode($usersArray, JSON_PRETTY_PRINT);
+            file_put_contents($usersArrayJSON);
+            sendJSON($message);
+        } else {
+            $message = ["id" => $user["id"], "username" => $user["username"], "firstTime" => false];
+            sendJSON($message);
+        }       
     } 
 }
+
+$errorMessage = ["message" => "Wrong username or password"];
+sendJSON($errorMessage, 404);
 
 ?>
