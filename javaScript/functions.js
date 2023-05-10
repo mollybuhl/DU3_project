9798251dc8a986ac46eb18dd1dacda656d2e5526
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-05-10 11:38:24 +0200

    updated fetch function

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -1,4 +1,20 @@
-async function fetchAPI(request){
-    const response = await fetch(request);
-    return await response.json();
+/* 
+isGET = true or false
+requestOptions = if it's a GET request, this should be a string with GET parameters. Any other method should be an ordinary options object
+*/
+
+async function fetchAPI(isGET, requestOptions){
+    try{
+        let request;
+        if(isGET){
+            request = new Request("php/api.php?" + requestOptions);
+        }else{
+            request = new Request("php/api.php", requestOptions);
+        }
+    
+        const response = await fetch(request);
+        return response;
+    }catch(error){
+        // Display the error with a popup
+    }
 }
