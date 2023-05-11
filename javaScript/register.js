d611547c35b9f35bb1ed619af571c4fcd295b098
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-05-11 15:17:46 +0200

    Fixed register to work with new API configurations

--- javaScript/register.js
+++ javaScript/register.js
@@ -1,6 +1,9 @@
 "use strict";
 
 function renderRegisterPage(){
+    const userID = parseInt(window.localStorage.getItem("userId"));
+    const userPassword = window.localStorage.getItem("userPassword");
+
     const mainDom = document.querySelector("body > main");
     mainDom.classList.add("mainRegister");
     mainDom.innerHTML = `
@@ -20,40 +23,35 @@ function renderRegisterPage(){
 
     document.querySelector(".wrapper > button").addEventListener("click", onClickRegister);
     document.querySelector("#goToLogin").addEventListener("click", renderLoginPage);
-}
 
-async function onClickRegister(){
-    const username = document.querySelector("#registerUsername").value;
-    const password = document.querySelector("#registerPassword").value;
-
-    // The options sent with the register request.
-    const requestOptions = {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        })
-    };
-
-    try{
-
-        const request = new Request("../php/register.php", requestOptions);
-        let response = await fetch(request);
-        let resource = await response.json();
+    async function onClickRegister(){
+        const username = document.querySelector("#registerUsername").value;
+        const password = document.querySelector("#registerPassword").value;
     
-        const messageToUserDom = document.querySelector("#messageToUser");
+        // The options sent with the register request.
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                action: "register",
+                userID: userID,
+                userPassword: userPassword,
+                username: username,
+                password: password,
+            })
+        };
     
-        // If the response was unsuccessful for any reason, print the error message to the user. Otherwise tell the user their account has been created then redirect them to the login page.
-        if(!response.ok){
-            messageToUserDom.innerHTML = resource.message;
-        }else{
-            messageToUserDom.innerHTML = "Your account has been created! You will now be redirected to the login page.";
-            setTimeout(renderLoginPage, 3000);
-        }
-
-    }catch(error){
-        console.log(error);
+            let response = await fetchAPI(false, requestOptions);
+            let resource = await response.json();
+        
+            const messageToUserDom = document.querySelector("#messageToUser");
+        
+            // If the response was unsuccessful for any reason, print the error message to the user. Otherwise tell the user their account has been created then redirect them to the login page.
+            if(!response.ok){
+                messageToUserDom.innerHTML = resource.message;
+            }else{
+                messageToUserDom.innerHTML = "Your account has been created! You will now be redirected to the login page.";
+                setTimeout(renderLoginPage, 3000);
+            }
     }
-
 }
--- php/register.php
+++ php/register.php
@@ -1,87 +1,70 @@
 <?php
-require_once "functions.php";
+function register($data, $users){
+    require_once "functions.php";
 
-// Get the method used for the request, then check to see if it's allowed with a custom funciton (checkMethod).
-$requestMethod = $_SERVER["REQUEST_METHOD"];
-$allowed = ["POST"];
-checkMethod($requestMethod, $allowed);
-
-$filename =  __DIR__."/users.json";
-$users = [];
-
-// Check if file exists. If it doesn't, save $users within $filename. If it exists get contents from $filename then decode and save it in $users.
-if(!file_exists($filename)){
-    $json = json_encode($users);
-    file_put_contents($filename, $json);
-}else{
-    $json = file_get_contents($filename);
-    $users = json_decode($json, true);
-}
-
-// Get contents from the request and save them in their respective variable.
-$requestJSON = file_get_contents("php://input");
-$requestData = json_decode($requestJSON, true);
-
-$username = $requestData["username"];
-$password = $requestData["password"];
-
-// Check if the given username or password is shorter than 3 characters. If so, send a message with status 400(Bad request).
-if(strlen($username) < 3 or strlen($password) < 3){
-    $message = ["message" => "Both the username and password must be 3 characters or longer. Please try again."];
-    sendJSON($message, 400); 
-}
-
-// Check if the given username is already used by another user. If so, send a message with status 409(Conflict).
-foreach($users as $user){
-    if($user["username"] == $username){
-        $message = ["message" => "Sorry, that username is taken. Please try another"];
-        sendJSON($message, 409);
+    // Get the method used for the request, then check to see if it's allowed with a custom funciton (checkMethod).
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+    $allowed = ["POST"];
+    checkMethod($requestMethod, $allowed);
+    
+    $username = $data["username"];
+    $password = $data["password"];
+    
+    // Check if the given username or password is shorter than 3 characters. If so, send a message with status 400(Bad request).
+    if(strlen($username) < 3 or strlen($password) < 3){
+        $message = ["message" => "Both the username and password must be 3 characters or longer. Please try again."];
+        sendJSON($message, 400); 
     }
-}
-
-//Randomise profile image
-$folder = "../media/profile_imgs";
-$imageSources = scandir($folder);
-$images = [];
-
-foreach($imageSources as $imageSource){
-    if(str_contains($imageSource, ".jpg")){
-        array_push($images, $imageSource);
+    
+    // Check if the given username is already used by another user. If so, send a message with status 409(Conflict).
+    foreach($users as $user){
+        if($user["username"] == $username){
+            $message = ["message" => "Sorry, that username is taken. Please try another"];
+            sendJSON($message, 409);
+        }
     }
-}
-
-$count = count($images);
-$index = rand(0,($count - 1));
-
-$profilePicture = $images[$index];
-
-// Associative array of the newly created user that is added to the array $users.
-$newUser = [
-    "username" => $username,
-    "password" => $password,
-    "loggedFeelings" => [],
-    "friends" => [],
-    "friendRequests" => [],
-    "posts" => [],
-    "conversations" => [],
-    "profilePicture" => "../media/profile_imgs/$profilePicture"
-];
-
-// Add unique ID to new user
-$highestID = 0;
-foreach($users as $user){
-    if($user["id"] > $highestID){
-        $highestID = $user["id"];
+    
+    //Randomise profile image
+    $folder = "../media/profile_imgs";
+    $imageSources = scandir($folder);
+    $images = [];
+    
+    foreach($imageSources as $imageSource){
+        if(str_contains($imageSource, ".jpg")){
+            array_push($images, $imageSource);
+        }
     }
+    
+    $count = count($images);
+    $index = rand(0,($count - 1));
+    
+    $profilePicture = $images[$index];
+    
+    // Associative array of the newly created user that is added to the array $users.
+    $newUser = [
+        "username" => $username,
+        "password" => $password,
+        "loggedFeelings" => [],
+        "friends" => [],
+        "friendRequests" => [],
+        "posts" => [],
+        "profilePicture" => "../media/profile_imgs/$profilePicture"
+    ];
+    
+    // Add unique ID to new user
+    $highestID = 0;
+    foreach($users as $user){
+        if($user["id"] > $highestID){
+            $highestID = $user["id"];
+        }
+    }
+    $newUser["id"] = $highestID + 1;
+    
+    $users[] = $newUser;
+    
+    // Encode $users and save it in $filename, then send the username of the created user.
+    putInUsersJSON($users);
+    sendJSON($newUser["username"], 200);
+    
 }
-$newUser["id"] = $highestID + 1;
-
-$users[] = $newUser;
-
-// Encode $users and save it in $filename, then send the username of the created user.
-$json = json_encode($users, JSON_PRETTY_PRINT);
-file_put_contents($filename, $json);
-
-sendJSON($newUser["username"], 200);
-
 ?>
