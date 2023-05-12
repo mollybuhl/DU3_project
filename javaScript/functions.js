9fa598c80f9a4ff4316b666179a38684c07feaae
Ossian Dahlberg <ossiandahlberg@hotmail.com>
2023-05-12 15:41:56 +0200

    Popup when fetching information

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -1,9 +1,21 @@
 /* 
 isGET = true or false
 requestOptions = if it's a GET request, this should be a string with GET parameters. Any other method should be an ordinary options object
+createFetchModal = if true, create a modal informing that its fetching information, if false, don't create a modal.
 */
 
-async function fetchAPI(isGET, requestOptions){
+async function fetchAPI(isGET, requestOptions, createFetchModal = true){
+    const body = document.querySelector("body");
+    if(createFetchModal){
+        const fetchModal = document.createElement("div");
+        fetchModal.classList.add("fetchModal");
+        fetchModal.innerHTML = `
+        <div>
+            <div>Fetching information...</div>
+        </div>
+        `
+        body.appendChild(fetchModal);
+    }
     try{
         let request;
         if(isGET){
@@ -11,9 +23,29 @@ async function fetchAPI(isGET, requestOptions){
         }else{
             request = new Request("php/api.php", requestOptions);
         }
-    
         const response = await fetch(request);
-        return response;
+    
+        if(!response.ok){
+            if(body.querySelector(".fetchModal") === null){
+                const fetchModal = document.createElement("div");
+                fetchModal.classList.add("fetchModal");
+                body.appendChild(fetchModal);
+            }
+            
+            const fetchModal = body.querySelector(".fetchModal")
+            fetchModal.innerHTML = `
+            <div>
+                <div>Sorry something went wrong, please try again.</div>
+                <button>Close</button>
+            </div>
+            `
+            fetchModal.querySelector("div > button").addEventListener("click", event => fetchModal.remove());
+
+            return response;
+        }else{
+            body.querySelector(".fetchModal").remove();
+            return response;
+        }
     }catch(error){
         // Display the error with a popup
     }
